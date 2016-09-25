b833bd82bc11b9c68d0666c23c3068f4ab3a2e32
MyNameIsKippo <juanamichi@gmail.com>
2016-09-24 22:30:18 -0500

    Cleans up stuff, introduces RX observables

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ var lorebot = require('./lorebot');
 
 var settings = {
 	name : 'LoreBot',
-	token : process.env.BOT_API_KEY
+	//token : process.env.BOT_API_KEY
+	token : 'xoxb-76419008017-ZDbs1h0FAUNTeYAQiDhTrzTX'
 }
 
 lorebot.setup(settings);
